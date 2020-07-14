7f7c03af1e301b742a0bed98d7b67ebb1d5db15b
NsavelevJs <nsavelevJS@gmail.com>
2020-07-14 13:24:11 -0400

    Added some sead data and finished authentication for backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_action :set_user, only: [:show, :update, :destroy]
-
+  before_action :authorize_request, except: :create
   # GET /users
   def index
     @users = User.all
@@ -16,9 +16,9 @@ class UsersController < ApplicationController
   # POST /users
   def create
     @user = User.new(user_params)
-
     if @user.save
-      render json: @user, status: :created, location: @user
+      @token = encode({ user_id: @user.id, username: @user.username })
+      render json: { user: @user, token: @token }, status: :created
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -46,6 +46,6 @@ class UsersController < ApplicationController
 
     # Only allow a trusted parameter "white list" through.
     def user_params
-      params.require(:user).permit(:username, :password_digest)
+      params.require(:user).permit(:username, :password)
     end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+@user = User.create!(username:'NickS', password:'yesorno')
+@user = User.create!(username:'ColinM', password:'yesoryes')
+@user = User.create!(username:'BryceL', password:'noorno')
+
+@article = Article.create!(title:'Who are you today?',content:'Ut in qui amet amet duis excepteur id ipsum excepteur proident velit aute occaecat ex. Est irure labore anim cupidatat irure voluptate id deserunt do officia reprehenderit. Eu excepteur duis consectetur ad minim non nisi reprehenderit amet.',img_url:'https://i.imgur.com/LVyQbPX.jpg')
+@article = Article.create!(title:'= vs == vs ===',content:'Ut in qui amet amet duis excepteur id ipsum excepteur proident velit aute occaecat ex. Est irure labore anim cupidatat irure voluptate id deserunt do officia reprehenderit. Eu excepteur duis consectetur ad minim non nisi reprehenderit amet.',img_url:'https://i.imgur.com/3mYcQec.png')
+@article = Article.create!(title:'What is a byte?',content:'Ut in qui amet amet duis excepteur id ipsum excepteur proident velit aute occaecat ex. Est irure labore anim cupidatat irure voluptate id deserunt do officia reprehenderit. Eu excepteur duis consectetur ad minim non nisi reprehenderit amet.',img_url:'https://i.imgur.com/9By4veO.jpg')
